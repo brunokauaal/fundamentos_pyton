e95d546b8a5d01148161331f24bec60e6033c6a5
bruno <brunokauaalvesdecarvalho@gmail.com>
2024-10-24 00:20:44 -0300

    Atribuição Multipla

---
+++ atribuicao_multipla.py
@@ -0,0 +1,22 @@
+#Atribuição_múltipla = Nós permite atribuir variaveis ao mesmo tempo usando uma linha de codigo
+
+#exemplo
+
+#name = "Bruno"
+#age = 19
+#strong = True
+
+#Utilizando a atribuição
+
+name,age, strong = "Bruno", 19, True
+
+
+chocolate = acucar = leite = 10
+
+print(chocolate)
+print(acucar)
+print(leite)
+
+print(name)
+print(age)
+print(strong)
