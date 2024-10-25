3a103b7e50c38f209407a4cae5b51f4a072322b5
bruno <brunokauaalvesdecarvalho@gmail.com>
2024-10-24 22:35:05 -0300

    noções basicas de tipificações

---
+++ typecasting.py
@@ -0,0 +1,28 @@
+#Type casting = noções basicas de tipificação = converter o tipo de dados de um valor para outro tipo de dados =
+
+
+x = 1
+
+y= 2.0
+
+z= "3"
+
+
+# convertendo o numero inteiro para um numero flutuante float
+x= float (x)
+
+# convertendo o folat para inteiro
+y= int (y)
+
+# convertendo o string para inteiro
+z= int (z)
+
+print(x)
+#print(type(x))
+
+print(y)
+#print(type(y))
+
+#Como converti o z para um numeor inteiro ele irá realizar a operação matematica
+print(z*3)
+#print(type(z))
