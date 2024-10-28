a099de60db62409b0c5fbc46318fb8968032c294
bruno <brunokauaalvesdecarvalho@gmail.com>
2024-10-27 23:17:54 -0300

    Adicionando funçoes relacionadas a numero em python

---
+++ funcoes_matematicas.py
@@ -0,0 +1,35 @@
+#Funções Matematicas
+
+#importando o modulo da matematica
+import math
+
+pi = 3.14
+
+
+x= 1
+y= 2
+z= 3
+
+# estamos  utilizando a funçao de arredondamento
+print(round(pi))
+
+# Iremos arredondar para um numero inteiro
+print(math.ceil(pi))
+
+# Arredondando o pi para baixo
+print(math.floor(pi))
+
+#Esta funçao abs ira nos dizer o quao longe estamos de zero estamos seria uma abreviação de valor absoluto
+print(abs(pi))
+
+#Aumentara um numero base elevado a uma potencia exemplo: (pi , elvado a 2)
+print(pow(pi,2))
+
+#Raiz quadrada
+print(math.sqrt(4))
+
+#Descobrir o maior valor
+print(max(x,y,z))
+
+#Descobrir o menor valor
+print(min(x,y,z))
