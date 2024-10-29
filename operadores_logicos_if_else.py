7e80cb13050a33d024f856f31898a4762616b0c7
bruno <brunokauaalvesdecarvalho@gmail.com>
2024-10-28 23:13:42 -0300

    Operadores lógicos (e,ou,nao) para if and  else

---
+++ operadores_logicos_if_else.py
@@ -0,0 +1,12 @@
+#Operadores lógicos (e,ou,não) = usados para verificar se duas ou mais declarações de condição]
+
+
+temp = int(input("Como esta a temperatura la fora hoje?: "))
+
+
+if temp >= 0 and temp <= 30:
+    print("A temperatura esta boa hoje!")
+    print("Vai pra fora!!!")
+elif temp < 0 or temp > 30:
+    print("Melhor ficar em casa hoje!")
+    print("Fique dentro de casa!")
